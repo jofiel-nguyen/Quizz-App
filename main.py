cde63c6e092e640d2582a38eaa05d9428d5f032b
Nhu-Nguyen <zennguyen2511@gmail.com>
2024-02-26 14:20:46 -0600

    adding requirements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import flask
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -8,7 +9,6 @@ for question in question_data:
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
 
 quiz = QuizBrain(question_bank)
 
